267431211e78955daac86d57f39f9f3746131287
Robert Crowdis <RobertCrowdis@gmail.com>
2020-06-17 15:00:02 -0400

    feat: add example 4

--- bdm.js
+++ bdm.js
@@ -1,7 +1,11 @@
 // Example 1 - reversing text
 document.getElementById('e1-btn').addEventListener('click', reverseText);
-const squares = document.getElementsByClassName('e2-square');
 
+
+// Example 4 - using binding for scope
+document.querySelector('#e4-btn').addEventListener('click', swapSquareColor)
+
+const squares = document.getElementsByClassName('e2-square');
 for (const square of squares) {
   square.addEventListener('click', blockColorChanger(square.classList[1]))
 }
@@ -22,4 +26,21 @@ function blockColorChanger(color) {
     const e2Text = document.getElementById('e2-text');
     e2Text.style.color = color;
   }
+}
+
+// Example 4 - Queryselector to swap color of a square.
+function swapSquareColor() {
+  const COLORCYCLE = { 'red': 'blue', 'blue': 'green', 'green': 'yellow', 'yellow': 'purple', 'purple': 'red' }
+
+  // Get required elements.
+  const e4Color = document.querySelector('.e4-color');
+  const currentColor = e4Color.textContent.toLowerCase();
+  let nextColor = COLORCYCLE[currentColor];
+  
+  const e4Square = document.querySelector('#e4-square');
+
+  // Set required elements.
+  e4Square.className = `${currentColor}`;
+  e4Color.innerText = nextColor.charAt(0).toUpperCase() + nextColor.slice(1);
+
 }
